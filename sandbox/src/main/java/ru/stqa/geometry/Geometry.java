e9313ab74b778d7f137aa720da8722214f6c126d
BukreevaAnna <arsentevaanna82@gmail.com>
2025-10-26 18:54:29 +0300

    Формула периметра

--- sandbox/src/main/java/ru/stqa/geometry/Geometry.java
+++ sandbox/src/main/java/ru/stqa/geometry/Geometry.java
@@ -11,7 +11,8 @@ public class Geometry {
 
         Rectangle.printRectangleArea(3.0, 5.0);
 
-        Triangle.printTriangleArea(3.0, 4.0, 6.0);
+        Triangle.printTriangleArea(3, 4, 6);
+        Triangle.printTranglePerimeter(3, 4, 6);
     }
 
 
--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -3,7 +3,7 @@ package ru.stqa.geometry.figures;
 public class Triangle {
 
     public static void printTriangleArea(double a, double b, double c) {
-        String text = String.format("Площадь треугольника со сторонами %f, %f и %f = %f", a, b,c, TriangleArea(a, b, c));
+        String text = String.format("Площадь труегольника со сторонами %f, %f и %f = %f", a, b,c, TriangleArea(a, b, c));
         System.out.println(text);
 
 
@@ -11,5 +11,15 @@ public class Triangle {
 
     private static double TriangleArea(double a, double b, double c) {
         return (a+b+c)/2;
+    }
+
+
+    public static void printTranglePerimeter(double a, double b, double c) {
+        String text = String.format("Периметр треугольника со сторонами %f, %f и %f = %f", a, b,c, TrianglePerimeter(a, b, c));
+        System.out.println(text);
+    }
+
+    private static double TrianglePerimeter(double a, double b, double c) {
+        return a + b + c;
     }
 }
