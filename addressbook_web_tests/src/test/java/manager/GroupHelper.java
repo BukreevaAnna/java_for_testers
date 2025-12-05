7e2fa66234ef8e854c68a278db73ee09840bed5d
BukreevaAnna <arsentevaanna82@gmail.com>
2025-12-05 12:30:42 +0300

    Удаление всех групп с помощью цикла

--- addressbook_web_tests/src/test/java/manager/GroupHelper.java
+++ addressbook_web_tests/src/test/java/manager/GroupHelper.java
@@ -26,7 +26,7 @@ public class GroupHelper extends HelperBase{
     public void removeGroup() {
         openGroupsPage();
         selectGroup();
-        removeSelectedGroup();
+        removeSelectedGroups();
         returnToGroupsPage();
     }
 
@@ -38,7 +38,7 @@ public class GroupHelper extends HelperBase{
         submitGroupModification();
         returnToGroupsPage();
     }
-    private void removeSelectedGroup() {
+    private void removeSelectedGroups() {
         click(By.name("delete"));
     }
 
@@ -80,5 +80,18 @@ public class GroupHelper extends HelperBase{
     public int getCount() {
         openGroupsPage();
         return manager.driver.findElements(By.name("selected[]")).size();
+    }
+
+    public void removeAllGroups() {
+        openGroupsPage();
+        selectAllGroups();
+        removeSelectedGroups();
+    }
+
+    private void selectAllGroups() {
+        var checkboxes = manager.driver.findElements(By.name("selected[]"));
+        for (var checkbox : checkboxes){
+            checkbox.click();
+        }
     }
 }
