5be416a81d71be948685e50cc9fe9e98a7213d3b
BukreevaAnna <arsentevaanna82@gmail.com>
2025-12-27 15:41:17 +0300

    №14: Реализовать проверку данных, загружаемых из БД

--- addressbook_web_tests/src/test/java/tests/Contacts/ContactModificationTests.java
+++ addressbook_web_tests/src/test/java/tests/Contacts/ContactModificationTests.java
@@ -14,16 +14,16 @@ public class ContactModificationTests extends TestBase {
     @Test
     void canModifyContact() {
         if (app.contacts().getCount() == 0){
-            app.contacts().createContact(new ContactData("", "first name", "last name","", "email", ""));
+            app.contacts().createContact(new ContactData("", "first name", "last name","", "email@gmail.com", randomFile("src/test/resources/images")));
         }
-        var oldContacts = app.contacts().getList();
+        var oldContacts = app.hbm().getContactList();
         var rnd = new Random();
         var index = rnd.nextInt(oldContacts.size());
-        var testData = new ContactData().withFirstName("modifiedName");
+        var testData = new ContactData().withFirstName("modifiedName").withPhoto(randomFile("src/test/resources/images"));
         app.contacts().modifyContact(oldContacts.get(index), testData);
-        var newContacts = app.contacts().getList();
+        var newContacts = app.hbm().getContactList();
         var expectedList = new ArrayList<>(oldContacts);
-        expectedList.set(index, testData.withId(oldContacts.get(index).id()));
+        expectedList.set(index, testData.withId(oldContacts.get(index).id()).withPhoto(""));
         Comparator<ContactData> compareById = (o1, o2) -> {
             return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
         };
